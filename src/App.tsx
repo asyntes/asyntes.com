827ac2b7d09d002b3ea68587bbc998a472ca04a7
antonio.santese <antonio.santese@cetma.it>
2025-07-28 15:06:25 +0200

    wip init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/asyntes.svg'
 import './App.css'
 
 function App() {
