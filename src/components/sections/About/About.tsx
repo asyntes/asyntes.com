1a61fb87393d54218fefe07e19f35e7d6b9e87ca
antonio.santese <antonio.santese@cetma.it>
2025-07-29 12:51:24 +0200

    Update About.tsx

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -12,17 +12,17 @@ const About = () => {
                             <div className="skill-item">
                                 <Code className="skill-icon" />
                                 <h4>Web Development</h4>
-                                <p>React, TypeScript, Node.js, moderne architetture web</p>
+                                <p>React, TypeScript, Spring Boot, Java, Docker, MongoDB, SQL, ecc.</p>
                             </div>
                             <div className="skill-item">
                                 <Brain className="skill-icon" />
                                 <h4>Intelligenza Artificiale</h4>
-                                <p>Machine Learning, AI integration, soluzioni intelligenti</p>
+                                <p>Machine Learning, AI integration, Chatbot, Preditctive Maintenance</p>
                             </div>
                             <div className="skill-item">
                                 <Glasses className="skill-icon" />
                                 <h4>Realtà Immersive</h4>
-                                <p>VR/AR, Three.js, esperienze interattive 3D</p>
+                                <p>Virtual Reality, Augmented Reality, Unity 3D, Three.js</p>
                             </div>
                             <div className="skill-item">
                                 <Palette className="skill-icon" />
